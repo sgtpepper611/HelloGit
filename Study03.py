dc30f3b90e19323ccb5b00c3bbb3fc054aa2a035
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-12 11:38:02 +0900

    基礎編-文字列

---
+++ Study03.py
@@ -0,0 +1,76 @@
+print('python-izm')
+
+print("python-izm")
+
+test_str = """
+test_1
+test_2
+"""
+print(test_str)
+
+test_str = 'python'
+test_str = test_str + '_'
+test_str = test_str + 'izm'
+
+print(test_str)
+
+test_str = '012' * 3
+test_str += '345'
+test_str += '678'
+test_str += '9'
+
+print(test_str)
+
+test_integer = 100
+
+print(str(test_integer) + '円')
+
+test_str = 'python-izm'
+
+print(test_str.replace('izm', 'ism'))
+
+test_str = 'python-izm'
+
+print(test_str.split('-'))
+
+test_str = '1234'
+
+print(test_str.rjust(10, '0'))
+print(test_str.rjust(10, '!'))
+
+test_str = '1234'
+
+print(test_str.zfill(10))
+print(test_str.zfill(3))
+
+test_str = 'python-izm'
+
+print(test_str.startswith('python'))
+print(test_str.startswith('izm'))
+
+test_str = 'Python-Izm.Com'
+
+print(test_str.upper())
+print(test_str.lower())
+
+print('----------------------------------------')
+
+test_str = '                python-izm.com'
+print(test_str)
+
+test_str = test_str.lstrip()
+print(test_str)
+
+test_str = test_str.lstrip('python')
+print(test_str)
+
+print('----------------------------------------')
+
+test_str = 'python-izm.com      '
+print(test_str + '/')
+
+test_str = test_str.rstrip()
+print(test_str + '/')
+
+test_str = test_str.rstrip("com")
+print(test_str)
