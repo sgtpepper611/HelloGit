b86d60194c02b5946b3e490099b5672f6eb44604
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-12 21:25:15 +0900

    10 基礎編-スライス

---
+++ Study10.py
@@ -0,0 +1,36 @@
+test_list_1 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_1[:])
+print(test_list_1[::])
+
+test_list_2 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_2[:4])
+
+test_list_3 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_3[2:])
+
+test_list_4 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_4[::2])
+
+test_list_5 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_5[3:5])
+
+test_list_6 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_6[-1:])   # 末尾から全ての要素
+print(test_list_6[:-1])   # 末尾まで全ての要素
+print(test_list_6[::-1])  # 末尾から全ての逆順要素
+
+test_list_7 = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list_7[:999])
+
+test_list_8 = ['https', 'www', 'python', 'izm', 'com']
+ 
+test_list_8[1:3] = ('WWW', 'PYTHON') #要素の代入
+ 
+print(test_list_8)
