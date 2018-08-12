1c6402726f3af270b6209e509cdb5130303c0f31
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-12 22:06:58 +0900

    13 基礎編-インポート(2)

---
+++ Study13.py
@@ -0,0 +1,10 @@
+import Study12
+ 
+test_class_1 = Study12.TestClass()
+test_class_1.test_method('1')
+ 
+ 
+from Study12 import TestClass
+ 
+test_class_2 = TestClass()
+test_class_2.test_method('2')
