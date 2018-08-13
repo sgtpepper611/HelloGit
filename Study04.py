2edd6bcf29ad62290928232fd6de7b1803a3b9cd
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-13 20:38:04 +0900

    04 基礎編-数値

--- Study04.py
+++ Study04.py
@@ -1,23 +1,23 @@
-test_integer = 100
+test_integer = 1000
 
 print(test_integer + 10)     # 加算（足し算）
 print(test_integer - 10)     # 減算（引き算）
 print(test_integer * 10)     # 乗算（掛け算）
 print(test_integer / 10)     # 除算（割り算）
 
-test_str = '100'
+test_str = '1000'
 
-print(int(test_str) + 100)
+print(int(test_str) + 1000)
 
-test_str = '100.5'
+test_str = '1000.5'
 
-print(float(test_str) + 100)
+print(float(test_str) + 1000)
 
 test_float = .5
 
 print(test_float)
 
-test_complex = 100 + 5j
+test_complex = 1000 + 5j
 
 print(test_complex)
 print(test_complex.real)
