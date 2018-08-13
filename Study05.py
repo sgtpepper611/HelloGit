3df48cad7bd595fd3d00624d3c91764cb4cb57b4
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-13 20:38:30 +0900

    基礎編-日付・時間

--- Study05.py
+++ Study05.py
@@ -39,12 +39,12 @@ print(today)
 # 明日の日付
 print(today + datetime.timedelta(days=1))
 
-newyear = datetime.datetime(2010, 1, 1)
+newyear = datetime.datetime(2018, 1, 1)
 
-# 2010年1月1日の一週間後
+# 2018年1月1日の一週間後
 print(newyear + datetime.timedelta(days=7))
 
-# 2010年1月1日から今日までの日数
+# 2018年1月1日から今日までの日数
 calc = today - newyear
 
 # 計算結果の戻り値は「timedelta」
@@ -56,5 +56,5 @@ print(calendar.isleap(2015))
 print(calendar.isleap(2016))
 print(calendar.isleap(2017))
 
-print(calendar.leapdays(2010, 2020))
+print(calendar.leapdays(2010, 2030))
 
