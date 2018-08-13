1b239a1764ef4ffc015c0e7d59dd37de3d8ac6d9
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-13 20:40:11 +0900

    09 基礎編-セット

--- Study09.py
+++ Study09.py
@@ -1,4 +1,4 @@
-test_set_1 = {'python', '-', 'izm', '.', 'com'}
+test_set_1 = {'python', '-', 'sgt', '.', 'com'}
 print(test_set_1)
 
 print('--------------------------------')
@@ -15,7 +15,7 @@ test_set = {'python'}
 # 空のセットは「set」を使う
 empty_set = set()
 
-test_set_2 = {'python', '-', 'izm', '.', 'com', 'python', 'izm'}
+test_set_2 = {'python', '-', 'sgt', '.', 'com', 'python', 'sgt'}
 
 print('--------------------------------')
 
@@ -25,18 +25,18 @@ for i in test_set_1:
 test_set_3 = set()
 
 test_set_3.add('python')
-test_set_3.update({'-', 'izm', '.', 'com'})
+test_set_3.update({'-', 'sgt', '.', 'com'})
  
 print(test_set_3)
 
-test_set_4 = {'python', '-', 'izm', '.', 'com'}
+test_set_4 = {'python', '-', 'sgt', '.', 'com'}
  
 test_set_4.remove('-')
 test_set_4.discard('.')
  
 print(test_set_4)
 
-test_set_5 = frozenset({'python', '-', 'izm', '.', 'com'})
+test_set_5 = frozenset({'python', '-', 'sgt', '.', 'com'})
  
 # test_set_5.remove('-') はAttributeErrorが発生する。
 # test_set_5.discard('.')はAttributeErrorが発生する。
