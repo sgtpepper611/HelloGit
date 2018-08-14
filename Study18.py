264ed5f5f911ed6313ce4dcfdbdc00b44aa10cdd
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-15 06:10:54 +0900

    18 基礎編-while文

---
+++ Study18.py
@@ -0,0 +1,14 @@
+counter = 0
+ 
+while counter < 15:
+    counter += 1
+    print(counter)
+
+
+counter = 0
+ 
+while True:
+    counter += 1
+    print(counter)
+    if counter == 18:
+        break
