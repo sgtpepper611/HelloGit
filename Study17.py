91208dd808e7e3d1581e4b4d589c20ab6e8f6c42
Sgtpepper611 <sgtpepper611@yahoo.co.jp>
2018-08-14 19:44:47 +0900

    17 基礎編-for文

---
+++ Study17.py
@@ -0,0 +1,18 @@
+for_sample = []
+for_sample.append("python")
+for_sample.append("-")
+for_sample.append("sgt")
+ 
+for value in for_sample:
+    print(value)
+
+for value in 'ABCDEF':
+    print(value)
+
+test_list_1 = [['リンゴ', 'バナナ'], ['メロン', 'イチゴ']]
+ 
+for value in test_list_1:
+    print(value)
+ 
+for value_1, value_2 in test_list_1:
+    print(value_1, value_2)
